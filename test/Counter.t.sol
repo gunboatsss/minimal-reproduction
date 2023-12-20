05abebf61fe1fa2747ba0330c1bf5367cf89d0d9
GUNBOATs <92352063+gunboatsss@users.noreply.github.com>
2023-12-20 20:12:11 +0700

    add case

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -7,10 +7,16 @@ import {OpsProxyFactory} from "./interfaces/OpsProxyFactory.sol";
 contract CounterTest is Test {
     OpsProxyFactory ops = OpsProxyFactory(0x44bde1bccdD06119262f1fE441FBe7341EaaC185);
     address target = 0xd55caA93dAeb8D4F846C4936E3D7E402EE952b70;
-    function setUp() public {
-        vm.createSelectFork(vm.envString("OP_MAINNET_RPC"), 18634930);
+    function test_prank() public {
+        vm.createSelectFork(vm.envString("OP_MAINNET_RPC"));
+        (address dedicatedMsgSender, bool deployed) = ops.getProxyOf(target);
+        if (!deployed) {
+            ops.deployFor(target);
+        }
+        vm.prank(target);
     }
     function test_prankfailed() public {
+        vm.createSelectFork(vm.envString("OP_MAINNET_RPC"), 18634930);
         (address dedicatedMsgSender, bool deployed) = ops.getProxyOf(target);
         if (!deployed) {
             ops.deployFor(target);
