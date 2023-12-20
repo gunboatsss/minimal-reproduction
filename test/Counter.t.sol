a18d4aa7de6280a54b4a2b5ea408c7c4b7d6af42
GUNBOATs <92352063+gunboatsss@users.noreply.github.com>
2023-12-20 20:09:37 +0700

    minimal reproduction of bug

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,19 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {OpsProxyFactory} from "./interfaces/OpsProxyFactory.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
-
+    OpsProxyFactory ops = OpsProxyFactory(0x44bde1bccdD06119262f1fE441FBe7341EaaC185);
+    address target = 0xd55caA93dAeb8D4F846C4936E3D7E402EE952b70;
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.createSelectFork(vm.envString("OP_MAINNET_RPC"), 18634930);
     }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_prankfailed() public {
+        (address dedicatedMsgSender, bool deployed) = ops.getProxyOf(target);
+        if (!deployed) {
+            ops.deployFor(target);
+        }
+        vm.prank(target);
     }
 }
